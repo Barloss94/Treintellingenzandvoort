64c6bcfc24fabda77c4dfbe7177dc3ac7c992173
Barloss94 <djbarry.0229@gmail.com>
2026-07-31 15:26:25 +0200

    Update service worker cache and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,105 @@
-const CACHE_NAME = "treintellingen-zandvoort-v1";
-const APP_ASSETS = [
+const CACHE_NAME = "treintellingen-zandvoort-v4";
+
+const APP_FILES = [
     "./",
     "./index.html",
     "./manifest.webmanifest",
     "./icons/icon-192.png",
-    "./icons/icon-512.png",
-    "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"
+    "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS))
+        caches
+            .open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_FILES))
     );
+
     self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-        ))
+        caches
+            .keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames
+                        .filter(cacheName => {
+                            return cacheName !== CACHE_NAME;
+                        })
+                        .map(cacheName => {
+                            return caches.delete(cacheName);
+                        })
+                );
+            })
+            .then(() => self.clients.claim())
     );
-    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-    if (event.request.method !== "GET") return;
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    const requestUrl = new URL(event.request.url);
+
+    if (requestUrl.origin !== self.location.origin) {
+        return;
+    }
+
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    const responseClone = response.clone();
+
+                    caches
+                        .open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(
+                                "./index.html",
+                                responseClone
+                            );
+                        });
+
+                    return response;
+                })
+                .catch(() => {
+                    return caches.match("./index.html");
+                })
+        );
+
+        return;
+    }
 
     event.respondWith(
-        caches.match(event.request).then(cached => {
-            if (cached) return cached;
+        caches.match(event.request)
+            .then(cachedResponse => {
+                const networkResponse = fetch(event.request)
+                    .then(response => {
+                        if (
+                            response &&
+                            response.status === 200 &&
+                            response.type === "basic"
+                        ) {
+                            const responseClone =
+                                response.clone();
 
-            return fetch(event.request).then(response => {
-                const copy = response.clone();
-                caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-                return response;
-            }).catch(() => caches.match("./index.html"));
-        })
+                            caches
+                                .open(CACHE_NAME)
+                                .then(cache => {
+                                    cache.put(
+                                        event.request,
+                                        responseClone
+                                    );
+                                });
+                        }
+
+                        return response;
+                    });
+
+                return cachedResponse || networkResponse;
+            })
     );
 });
